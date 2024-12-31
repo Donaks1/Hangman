2d1430c9b7c2fbbdd14549370bf43dbecf8584b1
donaks <donaks@tdfygu.com>
2024-12-31 12:17:58 +0800

    Usable Keyboard Buttons

--- script.js
+++ script.js
@@ -102,10 +102,6 @@ document.addEventListener('keydown', (event) => {
     testdiv.textContent = typedText;
 });
 
-function isWord(word) {
-    return true;
-}
-
 function checkWin(correctWord, typedText) {
     if (typedText.toUpperCase() === correctWord.toUpperCase()) {
 
@@ -131,3 +127,317 @@ function checkLetters(correctWord, typedText, row) {
         }
     }
 }
+
+const aButton = document.getElementById("a");
+const bButton = document.getElementById("b");
+const cButton = document.getElementById("c");
+const dButton = document.getElementById("d");
+const eButton = document.getElementById("e");
+const fButton = document.getElementById("f");
+const gButton = document.getElementById("g");
+const hButton = document.getElementById("h");
+const iButton = document.getElementById("i");
+const jButton = document.getElementById("j");
+const kButton = document.getElementById("k");
+const lButton = document.getElementById("l");
+const mButton = document.getElementById("m");
+const nButton = document.getElementById("n");
+const oButton = document.getElementById("o");
+const pButton = document.getElementById("p");
+const qButton = document.getElementById("q");
+const rButton = document.getElementById("r");
+const sButton = document.getElementById("s");
+const tButton = document.getElementById("t");
+const uButton = document.getElementById("u");
+const vButton = document.getElementById("v");
+const wButton = document.getElementById("w");
+const xButton = document.getElementById("x");
+const yButton = document.getElementById("y");
+const zButton = document.getElementById("z");
+const backspaceButton = document.getElementById("backspace");
+const enterButton = document.getElementById("enter");
+
+backspaceButton.addEventListener("click", function() {
+    typedText = typedText.slice(0, -1); 
+    outputName = "output" + row + position;
+    let outputDiv = document.getElementById(outputName);
+    outputDiv.textContent = '';
+    position--;
+});
+
+enterButton.addEventListener("click", function () {
+    checkWord(typedText).then((data) => {
+        if (data.title === "No Definitions Found") {
+            alert("Not a real word :(");
+        } 
+        else {
+            checkWin(correctWord, typedText);
+            checkLetters(correctWord, typedText, row);
+            
+            row++;
+            position = 0;
+            typedText = '';
+        }
+    });
+});
+
+aButton.addEventListener("click", function() {
+    if (position < 5) {
+        typedText += "A";
+        position++;
+        outputName = "output" + row + position;
+        let outputDiv = document.getElementById(outputName);
+        outputDiv.textContent = "A";
+    }
+});
+
+bButton.addEventListener("click", function() {
+    if (position < 5) {
+        typedText += "B";
+        position++;
+        outputName = "output" + row + position;
+        let outputDiv = document.getElementById(outputName);
+        outputDiv.textContent = "B";
+    }
+});
+
+cButton.addEventListener("click", function() {
+    if (position < 5) {
+        typedText += "C";
+        position++;
+        outputName = "output" + row + position;
+        let outputDiv = document.getElementById(outputName);
+        outputDiv.textContent = "C";
+    }
+});
+
+dButton.addEventListener("click", function() {
+    if (position < 5) {
+        typedText += "D";
+        position++;
+        outputName = "output" + row + position;
+        let outputDiv = document.getElementById(outputName);
+        outputDiv.textContent = "D";
+    }
+});
+
+eButton.addEventListener("click", function() {
+    if (position < 5) {
+        typedText += "E";
+        position++;
+        outputName = "output" + row + position;
+        let outputDiv = document.getElementById(outputName);
+        outputDiv.textContent = "E";
+    }
+});
+
+fButton.addEventListener("click", function() {
+    if (position < 5) {
+        typedText += "F";
+        position++;
+        outputName = "output" + row + position;
+        let outputDiv = document.getElementById(outputName);
+        outputDiv.textContent = "F";
+    }
+});
+
+gButton.addEventListener("click", function() {
+    if (position < 5) {
+        typedText += "G";
+        position++;
+        outputName = "output" + row + position;
+        let outputDiv = document.getElementById(outputName);
+        outputDiv.textContent = "G";
+    }
+});
+
+hButton.addEventListener("click", function() {
+    if (position < 5) {
+        typedText += "H";
+        position++;
+        outputName = "output" + row + position;
+        let outputDiv = document.getElementById(outputName);
+        outputDiv.textContent = "H";
+    }
+});
+
+iButton.addEventListener("click", function() {
+    if (position < 5) {
+        typedText += "I";
+        position++;
+        outputName = "output" + row + position;
+        let outputDiv = document.getElementById(outputName);
+        outputDiv.textContent = "I";
+    }
+});
+
+jButton.addEventListener("click", function() {
+    if (position < 5) {
+        typedText += "J";
+        position++;
+        outputName = "output" + row + position;
+        let outputDiv = document.getElementById(outputName);
+        outputDiv.textContent = "J";
+    }
+});
+
+kButton.addEventListener("click", function() {
+    if (position < 5) {
+        typedText += "K";
+        position++;
+        outputName = "output" + row + position;
+        let outputDiv = document.getElementById(outputName);
+        outputDiv.textContent = "K";
+    }
+});
+
+lButton.addEventListener("click", function() {
+    if (position < 5) {
+        typedText += "L";
+        position++;
+        outputName = "output" + row + position;
+        let outputDiv = document.getElementById(outputName);
+        outputDiv.textContent = "L";
+    }
+});
+
+mButton.addEventListener("click", function() {
+    if (position < 5) {
+        typedText += "M";
+        position++;
+        outputName = "output" + row + position;
+        let outputDiv = document.getElementById(outputName);
+        outputDiv.textContent = "M";
+    }
+});
+
+nButton.addEventListener("click", function() {
+    if (position < 5) {
+        typedText += "N";
+        position++;
+        outputName = "output" + row + position;
+        let outputDiv = document.getElementById(outputName);
+        outputDiv.textContent = "N";
+    }
+});
+
+oButton.addEventListener("click", function() {
+    if (position < 5) {
+        typedText += "O";
+        position++;
+        outputName = "output" + row + position;
+        let outputDiv = document.getElementById(outputName);
+        outputDiv.textContent = "O";
+    }
+});
+
+pButton.addEventListener("click", function() {
+    if (position < 5) {
+        typedText += "P";
+        position++;
+        outputName = "output" + row + position;
+        let outputDiv = document.getElementById(outputName);
+        outputDiv.textContent = "P";
+    }
+});
+
+qButton.addEventListener("click", function() {
+    if (position < 5) {
+        typedText += "Q";
+        position++;
+        outputName = "output" + row + position;
+        let outputDiv = document.getElementById(outputName);
+        outputDiv.textContent = "Q";
+    }
+});
+
+rButton.addEventListener("click", function() {
+    if (position < 5) {
+        typedText += "R";
+        position++;
+        outputName = "output" + row + position;
+        let outputDiv = document.getElementById(outputName);
+        outputDiv.textContent = "R";
+    }
+});
+
+sButton.addEventListener("click", function() {
+    if (position < 5) {
+        typedText += "S";
+        position++;
+        outputName = "output" + row + position;
+        let outputDiv = document.getElementById(outputName);
+        outputDiv.textContent = "S";
+    }
+});
+
+tButton.addEventListener("click", function() {
+    if (position < 5) {
+        typedText += "T";
+        position++;
+        outputName = "output" + row + position;
+        let outputDiv = document.getElementById(outputName);
+        outputDiv.textContent = "T";
+    }
+});
+
+uButton.addEventListener("click", function() {
+    if (position < 5) {
+        typedText += "U";
+        position++;
+        outputName = "output" + row + position;
+        let outputDiv = document.getElementById(outputName);
+        outputDiv.textContent = "U";
+    }
+});
+
+vButton.addEventListener("click", function() {
+    if (position < 5) {
+        typedText += "V";
+        position++;
+        outputName = "output" + row + position;
+        let outputDiv = document.getElementById(outputName);
+        outputDiv.textContent = "V";
+    }
+});
+
+wButton.addEventListener("click", function() {
+    if (position < 5) {
+        typedText += "W";
+        position++;
+        outputName = "output" + row + position;
+        let outputDiv = document.getElementById(outputName);
+        outputDiv.textContent = "W";
+    }
+});
+
+xButton.addEventListener("click", function() {
+    if (position < 5) {
+        typedText += "X";
+        position++;
+        outputName = "output" + row + position;
+        let outputDiv = document.getElementById(outputName);
+        outputDiv.textContent = "X";
+    }
+});
+
+yButton.addEventListener("click", function() {
+    if (position < 5) {
+        typedText += "Y";
+        position++;
+        outputName = "output" + row + position;
+        let outputDiv = document.getElementById(outputName);
+        outputDiv.textContent = "Y";
+    }
+});
+
+zButton.addEventListener("click", function() {
+    if (position < 5) {
+        typedText += "Z";
+        position++;
+        outputName = "output" + row + position;
+        let outputDiv = document.getElementById(outputName);
+        outputDiv.textContent = "Z";
+    }
+});
+
