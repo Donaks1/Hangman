c151e08796c6f84802aedb455c3f7da3cae2b141
donaks <donaks@tdfygu.com>
2024-12-31 13:15:58 +0800

    removed scaling in meta tag

--- script.js
+++ script.js
@@ -460,13 +460,4 @@ zButton.addEventListener("click", function() {
     }
 });
 
-let lastTouchEnd = 0;
-
-document.addEventListener('touchend', (event) => {
-    const now = new Date().getTime();
-    if (now - lastTouchEnd <= 300) {
-        event.preventDefault();
-    }
-    lastTouchEnd = now;
-}, false);
 
