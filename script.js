db77424a9d541928fa08c5f612c83e304d93d54d
donaks <donaks@tdfygu.com>
2024-12-31 13:04:50 +0800

    test js to remove double tap zoom on mobile

--- script.js
+++ script.js
@@ -460,3 +460,13 @@ zButton.addEventListener("click", function() {
     }
 });
 
+let lastTouchEnd = 0;
+
+document.addEventListener('touchend', (event) => {
+    const now = new Date().getTime();
+    if (now - lastTouchEnd <= 300) {
+        event.preventDefault();
+    }
+    lastTouchEnd = now;
+}, false);
+
